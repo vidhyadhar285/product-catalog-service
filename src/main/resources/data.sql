6ed260725bcc0a4075c09f2e3fc35d7da67d6dc5
vidhyadhar <vidhya2014n@gmail.com>
2020-03-19 07:24:34 +0530

    initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,10 @@
+INSERT INTO brand(brandid,barandname) VALUES(1,'adidas');
+INSERT INTO brand(brandid,barandname) VALUES(2,'puma');
+INSERT INTO brand(brandid,barandname) VALUES(3,'jockey');
 
-insert into brand values(1,'adidas');
-insert into brand values(2,'puma');
-insert into brand values(3,'jockey');
-
-insert into category values(1,'pants');
-insert into category values(2,'socks');
-insert into category values(3,'caps');
+INSERT INTO category(categoryid,categoryname) VALUES(1,'pants');
+INSERT INTO category(categoryid,categoryname) VALUES(2,'socks');
+INSERT INTO category(categoryid,categoryname) VALUES(3,'caps');
 
 insert into product values(1,'cotton-scoks',150,'blue','11-99-56',1,2)
 insert into product values(2,'wollen-scoks',350,'green','11-99-57',1,2)
